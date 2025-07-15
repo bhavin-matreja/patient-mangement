337d75bacee55b37358763e3f407ab6ed71b9d92
bhavin <bhavin.matreja@gmail.com>
2025-07-15 22:31:51 +0530

    create jwt validation filter and apply filter in gateway

---
+++ api-gateway/src/main/java/com/bvn/api_gateway/filter/JwtValidationGatewayFilterFactory.java
@@ -0,0 +1,52 @@
+package com.bvn.api_gateway.filter;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Component
+public class JwtValidationGatewayFilterFactory extends
+        AbstractGatewayFilterFactory<Object> {
+
+    private final WebClient webClient;
+
+    public JwtValidationGatewayFilterFactory(WebClient.Builder webClientBuilder,
+                                             @Value("${auth.service.url}") String authServiceUrl) {
+        // taking auth service url as env variable as that can change locally and when deployed to aws
+        this.webClient = webClientBuilder.baseUrl(authServiceUrl).build();
+    }
+
+    /*
+    exchange - value passed by spring gateway that holds all properties of the request
+    chain - manages the chain of filters that currently exists in the filter chain i.e. we can have multiple filters
+
+     */
+    @Override
+    public GatewayFilter apply(Object config) {
+        return (exchange, chain) -> {
+            String token =
+                    exchange.getRequest().getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
+
+            if(token == null || !token.startsWith("Bearer ")) {
+                exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+                return exchange.getResponse().setComplete();
+            }
+
+            /*
+            make GET request through webclient to the auth uri
+            toBodilessEntity() - telling web client there is no body in response
+            .then(chain.filter(exchange)) - then continue the request in the chain if it auth is success
+             */
+            return webClient.get()
+                    .uri("/validate")
+                    .header(HttpHeaders.AUTHORIZATION, token)
+                    .retrieve()
+                    .toBodilessEntity()
+                    .then(chain.filter(exchange));
+        };
+    }
+}
