883eaf35e6899be485779922889a0ba4a6e4a0bf
bhavin <bhavin.matreja@gmail.com>
2025-09-17 21:30:47 +0530

    resolved issue in test for analytics-service

--- analytics-service/src/test/java/com/bvn/analytics_service/AnalyticsServiceApplicationTests.java
+++ analytics-service/src/test/java/com/bvn/analytics_service/AnalyticsServiceApplicationTests.java
@@ -3,11 +3,8 @@ package com.bvn.analytics_service;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class AnalyticsServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
